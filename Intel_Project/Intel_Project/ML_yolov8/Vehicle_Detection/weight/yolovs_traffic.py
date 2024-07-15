721be763e9abb3b82e72ccd821280d77a0daa8f3
Ironman-1899 <116809923+Ironman-1899@users.noreply.github.com>
2024-07-15 14:59:11 +0530

    This is the intel project files

---
+++ Intel_Project/Intel_Project/ML_yolov8/Vehicle_Detection/weight/yolovs_traffic.py
@@ -0,0 +1,53 @@
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+import urllib.request
+import numpy as np
+
+url = 'http://192.168.79.97/cam-hi.jpg'
+cv2.namedWindow("Live Cam Testing", cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Live Cam Testing', 1280, 720)
+cv2.moveWindow('Live Cam Testing', 0, 0)
+picam2 = cv2.VideoCapture(url)
+
+
+    
+if not picam2.isOpened():
+    print("Failed to open the IP camera stream")
+    exit()
+
+model = YOLO('/home/raspberry/Intel_Project/ML_yolov8/Vehicle_Detection/weight/yolov8s.pt')
+my_file = open("/home/raspberry/Intel_Project/ML_yolov8/Vehicle_Detection/weight/coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+count = 0
+
+while True:
+    img_resp = urllib.request.urlopen(url)
+    imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+    im = cv2.imdecode(imgnp, cv2.IMREAD_COLOR)
+    results = model.predict(im)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
+    
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+        if d < len(class_list):
+            c = class_list[d]
+            cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cvzone.putTextRect(im, f'{c}', (x1, y1), 1, 1)
+        else:
+            c = 'Unknown'
+            cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cvzone.putTextRect(im, f'{c}', (x1, y1), 1, 1)
+    
+    cv2.imshow("Live Cam Testing", im)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
---
+++ Intel_Project/Intel_Project/ML_yolov8/Vehicle_number_plate_detection/weight/advanced_plate_detection.py
@@ -0,0 +1,211 @@
+import ast
+import cv2 as cv
+import easyocr
+from glob import glob
+import numpy as np
+import pandas as pd
+import string
+from ultralytics import YOLO
+
+# regular pre-trained yolov8 model for car recognition
+# coco_model = YOLO('yolov8n.pt')
+coco_model = YOLO('yolov8s.pt')
+# yolov8 model trained to detect number plates
+np_model = YOLO('ML_yolov8/Vehicle_number_plate_detection/weight/license_plate_detector_best_zoom.pt')
+
+# read in test video paths
+videos = glob('/home/raspberry/Intel_Project/ML_yolov8/Vehicle_number_plate_detection/weight/vehicle_license_plate_video.mp4')
+print(videos)
+
+
+reader = easyocr.Reader(['en'], gpu=True)
+
+
+# Mapping dictionaries for character conversion
+# characters that can easily be confused can be 
+# verified by their location - an `O` in a place
+# where a number is expected is probably a `0`
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
+def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=6, line_length_x=200, line_length_y=200):
+    x1, y1 = top_left
+    x2, y2 = bottom_right
+
+    cv.line(img, (x1, y1), (x1, y1 + line_length_y), color, thickness)  #-- top-left
+    cv.line(img, (x1, y1), (x1 + line_length_x, y1), color, thickness)
+
+    cv.line(img, (x1, y2), (x1, y2 - line_length_y), color, thickness)  #-- bottom-left
+    cv.line(img, (x1, y2), (x1 + line_length_x, y2), color, thickness)
+
+    cv.line(img, (x2, y1), (x2 - line_length_x, y1), color, thickness)  #-- top-right
+    cv.line(img, (x2, y1), (x2, y1 + line_length_y), color, thickness)
+
+    cv.line(img, (x2, y2), (x2, y2 - line_length_y), color, thickness)  #-- bottom-right
+    cv.line(img, (x2, y2), (x2 - line_length_x, y2), color, thickness)
+
+    return img
+
+
+
+
+
+def license_complies_format(text):
+    # True if the license plate complies with the format, False otherwise.
+    if len(text) != 7:
+        return False
+
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+        return True
+    else:
+        return False
+    
+
+def format_license(text):
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
+               2: dict_char_to_int, 3: dict_char_to_int}
+    for j in [0, 1, 2, 3, 4, 5, 6]:
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
+
+
+def read_license_plate(license_plate_crop):
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+
+        # verify that text is conform to a standard license plate
+        if license_complies_format(text):
+            # bring text into the default license plate format
+            return format_license(text), score
+
+    return None, None
+
+def write_csv(results, output_path):
+    
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{},{},{},{},{}\n'.format(
+            'frame_number', 'track_id', 'car_bbox', 'car_bbox_score',
+            'license_plate_bbox', 'license_plate_bbox_score', 'license_plate_number',
+            'license_text_score'))
+
+        for frame_number in results.keys():
+            for track_id in results[frame_number].keys():
+                print(results[frame_number][track_id])
+                if 'car' in results[frame_number][track_id].keys() and \
+                   'license_plate' in results[frame_number][track_id].keys() and \
+                   'number' in results[frame_number][track_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{},{}\n'.format(
+                        frame_number,
+                        track_id,
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['car']['bbox'][0],
+                            results[frame_number][track_id]['car']['bbox'][1],
+                            results[frame_number][track_id]['car']['bbox'][2],
+                            results[frame_number][track_id]['car']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['car']['bbox_score'],
+                        '[{} {} {} {}]'.format(
+                            results[frame_number][track_id]['license_plate']['bbox'][0],
+                            results[frame_number][track_id]['license_plate']['bbox'][1],
+                            results[frame_number][track_id]['license_plate']['bbox'][2],
+                            results[frame_number][track_id]['license_plate']['bbox'][3]
+                        ),
+                        results[frame_number][track_id]['license_plate']['bbox_score'],
+                        results[frame_number][track_id]['license_plate']['number'],
+                        results[frame_number][track_id]['license_plate']['text_score'])
+                    )
+        f.close()
+
+
+
+results = {}
+
+# read video by index
+video = cv.VideoCapture(videos[0])
+
+ret = True
+frame_number = -1
+vehicles = [2,3,5]
+
+# read the entire video
+while ret:
+    ret, frame = video.read()
+    frame_number += 1
+    if ret:
+        results[frame_number] = {}  
+        # vehicle detector
+        detections = coco_model.track(frame, persist=True)[0]
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, track_id, score, class_id = detection
+            if int(class_id) in vehicles and score > 0.5:
+                vehicle_bounding_boxes = []
+                vehicle_bounding_boxes.append([x1, y1, x2, y2, track_id, score])
+                for bbox in vehicle_bounding_boxes:
+                    print(bbox)
+                    roi = frame[int(y1):int(y2), int(x1):int(x2)]
+                    
+                    # license plate detector for region of interest
+                    license_plates = np_model(roi)[0]
+                    # process license plate
+                    for license_plate in license_plates.boxes.data.tolist():
+                        plate_x1, plate_y1, plate_x2, plate_y2, plate_score, _ = license_plate
+                        # crop plate from region of interest
+                        plate = roi[int(plate_y1):int(plate_y2), int(plate_x1):int(plate_x2)]
+                        # de-colorize
+                        plate_gray = cv.cvtColor(plate, cv.COLOR_BGR2GRAY)
+                        # posterize
+                        _, plate_treshold = cv.threshold(plate_gray, 64, 255, cv.THRESH_BINARY_INV)
+                        cv.imwrite(str(track_id) + '_gray.jpg', plate_gray)
+                        cv.imwrite(str(track_id) + '_thresh.jpg', plate_treshold)
+                        # OCR
+                        np_text, np_score = read_license_plate(plate_treshold)
+                        # if plate could be read write results
+                        if np_text is not None:
+                            results[frame_number][track_id] = {
+                                'car': {
+                                    'bbox': [x1, y1, x2, y2],
+                                    'bbox_score': score
+                                },
+                                'license_plate': {
+                                    'bbox': [plate_x1, plate_y1, plate_x2, plate_y2],
+                                    'bbox_score': plate_score,
+                                    'number': np_text,
+                                    'text_score': np_score
+                                }
+                            }
+
+write_csv(results, './results.csv')
+video.release()
+
+results = pd.read_csv('./results.csv')
+
+# show results for tracking ID `1` - sort by OCR prediction confidence
+results[results['track_id'] == 1.].sort_values(by='license_text_score', ascending=False)
+
+
